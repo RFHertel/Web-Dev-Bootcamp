d9bbb56ed592e6081efb46405f3e0cb7a8e96b2e
RF <74387792+RFHertel@users.noreply.github.com>
2023-12-03 14:22:25 -0500

    Create app.js

---
+++ L54 - JS DOM querySelector Span Element Img and Give URL and Alt text/app.js
@@ -0,0 +1,5 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+
+const eggImg = document.querySelector('img');
+eggImg.src = 'https://devsprouthosting.com/images/chicken.jpg';
+eggImg.alt = 'chicken';
