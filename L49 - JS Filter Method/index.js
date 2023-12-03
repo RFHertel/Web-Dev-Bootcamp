88a4d9e28222ffde282def26edef1c1a717c36f3
RF <74387792+RFHertel@users.noreply.github.com>
2023-12-03 12:45:24 -0500

    Update and rename index.js to Ex49.md

--- L49 - JS Filter Method/index.js
+++
@@ -1,7 +0,0 @@
-//Filter User name length:
-
-// validUserNames(['mark', 'staceysmom1978', 'q29832128238983', 'carrie98', 'MoanaFan']);
-// // => ["mark", "carrie98", "MoanaFan"]
-
-
-const validUserNames = usernames => usernames.filter(user => user.length < 10);
