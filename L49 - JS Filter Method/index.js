ec03435ca2295d2a816fa0e0c1a02be3996e283e
RF <74387792+RFHertel@users.noreply.github.com>
2023-12-03 12:44:48 -0500

    Update index.js

--- L49 - JS Filter Method/index.js
+++ L49 - JS Filter Method/index.js
@@ -1,1 +1,7 @@
+//Filter User name length:
+
+// validUserNames(['mark', 'staceysmom1978', 'q29832128238983', 'carrie98', 'MoanaFan']);
+// // => ["mark", "carrie98", "MoanaFan"]
+
+
 const validUserNames = usernames => usernames.filter(user => user.length < 10);
