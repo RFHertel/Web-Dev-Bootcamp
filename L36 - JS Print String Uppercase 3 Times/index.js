734e45b4f0bdf55dd0c0b5e2b41a9c251998cf08
RF <74387792+RFHertel@users.noreply.github.com>
2023-12-03 11:42:21 -0500

    Create index.js

---
+++ L36 - JS Print String Uppercase 3 Times/index.js
@@ -0,0 +1,13 @@
+// DEFINE YOUR FUNCTION:
+function rant(message) {
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+}
+
+// Solution with a for loop:
+function rant(message) {
+  for (let i = 0; i < 3; i++) {
+    console.log(message.toUpperCase());
+  }
+}
