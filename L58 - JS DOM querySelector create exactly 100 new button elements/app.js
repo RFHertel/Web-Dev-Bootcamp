5e12f451ad2db753af20b2307a80ec11cd8341f3
RF <74387792+RFHertel@users.noreply.github.com>
2023-12-03 15:06:30 -0500

    Update app.js

--- L58 - JS DOM querySelector create exactly 100 new button elements/app.js
+++ L58 - JS DOM querySelector create exactly 100 new button elements/app.js
@@ -3,6 +3,6 @@ const container = document.querySelector('#container');
 
 for (let i = 0; i < 100; i++) {
   const btn = document.createElement('button');
-  btn.innerText = 'Click';
+  btn.innerText = 'Hey';
   container.appendChild(btn);
 }
