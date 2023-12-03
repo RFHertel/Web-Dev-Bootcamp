ab0510cb3232dcb63dfc53db6411111925485512
RF <74387792+RFHertel@users.noreply.github.com>
2023-12-03 14:45:49 -0500

    Create app.js

---
+++ L55 - JS DOM querySelector Manipulative Div Container and Img/app.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+container.style.textAlign = 'center';
+
+const image = document.querySelector("img");
+image.style.borderRadius = '50%';
+image.style.width = '150px';
+
+
