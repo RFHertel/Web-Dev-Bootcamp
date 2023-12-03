6171d2c21cae1437ab2d091a9e833b80a5e89bbe
RF <74387792+RFHertel@users.noreply.github.com>
2023-12-03 12:24:58 -0500

    Create index.js

---
+++ L48 - JS Arrow Function Exercise/index.js
@@ -0,0 +1,10 @@
+// Write an arrow function expression making:
+// greet("Hagrid") //"Hey Hagrid!" 
+// greet("Luna") //"Hey Luna!"
+
+// const greet = (name) => {
+//   return `Hey ${name}!`;
+// }
+
+//Otherway:
+const greet = name => `Hey ${name}!`;
