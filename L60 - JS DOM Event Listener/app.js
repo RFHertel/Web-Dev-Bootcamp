be54be75beae60784be9f7d4a32e2c0c6da0d88a
RF <74387792+RFHertel@users.noreply.github.com>
2023-12-03 15:13:07 -0500

    Create app.js

---
+++ L60 - JS DOM Event Listener/app.js
@@ -0,0 +1,9 @@
+const helloBtn = document.querySelector('#hello');
+helloBtn.addEventListener('click', function () {
+  console.log('hello');
+});
+
+const goodbyeBtn = document.querySelector('#goodbye');
+goodbyeBtn.addEventListener('click', function () {
+  console.log('goodbye');
+});
