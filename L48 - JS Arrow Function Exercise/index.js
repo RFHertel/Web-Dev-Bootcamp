ab3f771c6ccaaccd05c10d8413a853f06c1370a8
RF <74387792+RFHertel@users.noreply.github.com>
2023-12-03 12:27:14 -0500

    Update index.js

--- L48 - JS Arrow Function Exercise/index.js
+++ L48 - JS Arrow Function Exercise/index.js
@@ -2,9 +2,6 @@
 // greet("Hagrid") //"Hey Hagrid!" 
 // greet("Luna") //"Hey Luna!"
 
-// const greet = (name) => {
-//   return `Hey ${name}!`;
-// }
-
-//Otherway:
-const greet = name => `Hey ${name}!`;
+const greet = (name) => {
+  return `Hey ${name}!`;
+}
